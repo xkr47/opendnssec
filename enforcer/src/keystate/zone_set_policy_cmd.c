50f56583f5a105fc5c1c4c3d92baf57d73869b36
Jonas Berlin <xkr47@outerspace.dyndns.org>
2018-07-08 22:12:19 +0300

    Fix spaces -> tabs

--- enforcer/src/keystate/zone_set_policy_cmd.c
+++ enforcer/src/keystate/zone_set_policy_cmd.c
@@ -1,4 +1,4 @@
-/*
+ /*
  * Copyright (c) 2017 .SE (The Internet Infrastructure Foundation).
  * Copyright (c) 2017 OpenDNSSEC AB (svb)
  * All rights reserved.
@@ -52,8 +52,8 @@ usage(int sockfd)
 		"zone set-policy\n"
 		"	--zone <zone>				aka -z\n"
 		"	--policy <policy>			aka -p\n"
-    );
-    client_printf(sockfd,
+	);
+	client_printf(sockfd,
 		"	[--xml]					aka -u\n"
 	);
 }
@@ -61,13 +61,13 @@ usage(int sockfd)
 static void
 help(int sockfd)
 {
-    client_printf(sockfd,
-        "Change the policy of an existing zone in the enforcer database.\n"
-	"\nOptions:\n"
-        "zone		name of the zone\n"
-        "policy		name of the new policy\n"
-        "xml		update the zonelist.xml file\n\n"
-    );
+	client_printf(sockfd,
+		"Change the policy of an existing zone in the enforcer database.\n"
+		"\nOptions:\n"
+		"zone		name of the zone\n"
+		"policy		name of the new policy\n"
+		"xml		update the zonelist.xml file\n\n"
+	);
 }
 
 static int set_zone_policy(int sockfd, db_connection_t* dbconn, zone_db_t* zone, policy_t* policy) {
@@ -75,134 +75,134 @@ static int set_zone_policy(int sockfd, db_connection_t* dbconn, zone_db_t* zone,
 	int cmp;
 
 	if (db_value_cmp(zone_db_policy_id(zone), wanted_policy_id, &cmp)) {
-        client_printf_err(sockfd, "Unable to update zone, database error!\n");
+		client_printf_err(sockfd, "Unable to update zone, database error!\n");
 		return 1;
 	}
 	if (!cmp) {
-        client_printf_err(sockfd, "Policy same as before, not updating.\n");
+		client_printf_err(sockfd, "Policy same as before, not updating.\n");
 		return 0;
 	}
 
 	if (zone_db_set_policy_id(zone, wanted_policy_id)) {
-        client_printf_err(sockfd, "Unable to update zone, database error!\n");
+		client_printf_err(sockfd, "Unable to update zone, database error!\n");
 		return 1;
 	}
 
 	if (zone_db_update(zone)) {
-        client_printf(sockfd, "Failed to update zone in database.\n");
+		client_printf(sockfd, "Failed to update zone in database.\n");
 		return 1;
 	}
-    ods_log_info("[%s] zone %s policy updated to %s", module_str, zone_db_name(zone), policy_name(policy));
-    client_printf(sockfd, "Zone %s policy successfully set to %s\n", zone_db_name(zone), policy_name(policy));
+	ods_log_info("[%s] zone %s policy updated to %s", module_str, zone_db_name(zone), policy_name(policy));
+	client_printf(sockfd, "Zone %s policy successfully set to %s\n", zone_db_name(zone), policy_name(policy));
 	return 0;
 }
 
 static int
 run(int sockfd, cmdhandler_ctx_type* context, char *cmd)
 {
-    #define NARGV 18
-    const char* argv[NARGV];
-    int argc = 0;
-    const char *zone_name = NULL;
-    char *policy_name = NULL;
-    int write_xml = 0;
-    int long_index = 0, opt = 0;
-    int ret = 0;
-    char path[PATH_MAX];
-    db_connection_t* dbconn = getconnectioncontext(context);
-    engine_type* engine = getglobalcontext(context);
+	#define NARGV 18
+	const char* argv[NARGV];
+	int argc = 0;
+	const char *zone_name = NULL;
+	char *policy_name = NULL;
+	int write_xml = 0;
+	int long_index = 0, opt = 0;
+	int ret = 0;
+	char path[PATH_MAX];
+	db_connection_t* dbconn = getconnectioncontext(context);
+	engine_type* engine = getglobalcontext(context);
 
-    static struct option long_options[] = {
-        {"zone", required_argument, 0, 'z'},
-        {"policy", required_argument, 0, 'p'},
-        {"xml", no_argument, 0, 'u'},
-        {0, 0, 0, 0}
-    };
+	static struct option long_options[] = {
+		{"zone", required_argument, 0, 'z'},
+		{"policy", required_argument, 0, 'p'},
+		{"xml", no_argument, 0, 'u'},
+		{0, 0, 0, 0}
+	};
 
-    ods_log_debug("[%s] %s command", module_str, zone_set_policy_funcblock.cmdname);
+	ods_log_debug("[%s] %s command", module_str, zone_set_policy_funcblock.cmdname);
 
-    argc = ods_str_explode(cmd, NARGV, argv);
-    if (argc == -1) {
-        client_printf_err(sockfd, "too many arguments\n");
-        ods_log_error("[%s] too many arguments for %s command",
-                      module_str, zone_set_policy_funcblock.cmdname);
-        return -1;
-    }
+	argc = ods_str_explode(cmd, NARGV, argv);
+	if (argc == -1) {
+		client_printf_err(sockfd, "too many arguments\n");
+		ods_log_error("[%s] too many arguments for %s command",
+					  module_str, zone_set_policy_funcblock.cmdname);
+		return -1;
+	}
 
-    optind = 0;
-    while ((opt = getopt_long(argc, (char* const*)argv, "z:p:u", long_options, &long_index)) != -1) {
-        switch (opt) {
-            case 'z':
-                zone_name = optarg;
-                break;
-            case 'p':
+	optind = 0;
+	while ((opt = getopt_long(argc, (char* const*)argv, "z:p:u", long_options, &long_index)) != -1) {
+		switch (opt) {
+			case 'z':
+				zone_name = optarg;
+				break;
+			case 'p':
 				policy_name = strdup(optarg);
-                break;
-            case 'u':
-                write_xml = 1;
-                break;
-            default:
-                client_printf_err(sockfd, "unknown arguments\n");
-                ods_log_error("[%s] unknown arguments for %s command",
-                                module_str, zone_set_policy_funcblock.cmdname);
-                return -1;
-        }
-    }
+				break;
+			case 'u':
+				write_xml = 1;
+				break;
+			default:
+				client_printf_err(sockfd, "unknown arguments\n");
+				ods_log_error("[%s] unknown arguments for %s command",
+							  module_str, zone_set_policy_funcblock.cmdname);
+				return -1;
+		}
+	}
 
-    if (!zone_name) {
-        client_printf_err(sockfd, "expected option --zone <zone>\n");
+	if (!zone_name) {
+		client_printf_err(sockfd, "expected option --zone <zone>\n");
 		free(policy_name);
-        return -1;
-    } else if (!policy_name) {
-        client_printf_err(sockfd, "expected option --policy <policy>\n");
-        return -1;
-    }
+		return -1;
+	} else if (!policy_name) {
+		client_printf_err(sockfd, "expected option --policy <policy>\n");
+		return -1;
+	}
 
-    //validation
+	//validation
 
 	zone_db_t* zone = zone_db_new_get_by_name(dbconn, zone_name);
 	if (!zone) {
-        client_printf_err(sockfd, "Unable to update zone, zone does not exist!\n");
+		client_printf_err(sockfd, "Unable to update zone, zone does not exist!\n");
 		free(policy_name);
-        return 1;
-    }
+		return 1;
+	}
 
 	policy_t* policy = policy_new_get_by_name(dbconn, policy_name);
 	free(policy_name);
 	if (!policy) {
-        client_printf_err(sockfd, "Unable to update zone, policy does not exist!\n");
+		client_printf_err(sockfd, "Unable to update zone, policy does not exist!\n");
 		zone_db_free(zone);
 		return 1;
 	}
 
-    /* input looks okay, lets update the database */
+	/* input looks okay, lets update the database */
 	ret = set_zone_policy(sockfd, dbconn, zone, policy);
 
 	zone_db_free(zone);
 	policy_free(policy);
 
-    if (write_xml) {
-        if (zonelist_export(sockfd, dbconn, engine->config->zonelist_filename_enforcer, 1) != ZONELIST_EXPORT_OK) {
-            ods_log_error("[%s] zonelist exported to %s failed", module_str, engine->config->zonelist_filename_enforcer);
-            client_printf_err(sockfd, "Exported zonelist to %s failed!\n", engine->config->zonelist_filename_enforcer);
-            ret = 1;
-        } else {
-            ods_log_info("[%s] zonelist exported to %s successfully", module_str, engine->config->zonelist_filename_enforcer);
-            client_printf(sockfd, "Exported zonelist to %s successfully\n", engine->config->zonelist_filename_enforcer);
-        }
-    }
+	if (write_xml) {
+		if (zonelist_export(sockfd, dbconn, engine->config->zonelist_filename_enforcer, 1) != ZONELIST_EXPORT_OK) {
+			ods_log_error("[%s] zonelist exported to %s failed", module_str, engine->config->zonelist_filename_enforcer);
+			client_printf_err(sockfd, "Exported zonelist to %s failed!\n", engine->config->zonelist_filename_enforcer);
+			ret = 1;
+		} else {
+			ods_log_info("[%s] zonelist exported to %s successfully", module_str, engine->config->zonelist_filename_enforcer);
+			client_printf(sockfd, "Exported zonelist to %s successfully\n", engine->config->zonelist_filename_enforcer);
+		}
+	}
 
-    if (snprintf(path, sizeof(path), "%s/%s", engine->config->working_dir_enforcer, OPENDNSSEC_ENFORCER_ZONELIST) >= (int)sizeof(path)
-        || zonelist_export(sockfd, dbconn, path, 0) != ZONELIST_EXPORT_OK)
-    {
-        ods_log_error("[%s] internal zonelist update failed", module_str);
-        client_printf_err(sockfd, "Unable to update the internal zonelist %s, updates will not reach the Signer!\n", path);
-        ret = 1;
-    } else {
-        ods_log_info("[%s] internal zonelist updated successfully", module_str);
-    }
+	if (snprintf(path, sizeof(path), "%s/%s", engine->config->working_dir_enforcer, OPENDNSSEC_ENFORCER_ZONELIST) >= (int)sizeof(path)
+		|| zonelist_export(sockfd, dbconn, path, 0) != ZONELIST_EXPORT_OK)
+	{
+		ods_log_error("[%s] internal zonelist update failed", module_str);
+		client_printf_err(sockfd, "Unable to update the internal zonelist %s, updates will not reach the Signer!\n", path);
+		ret = 1;
+	} else {
+		ods_log_info("[%s] internal zonelist updated successfully", module_str);
+	}
 
-    return ret;
+	return ret;
 }
 
 struct cmd_func_block zone_set_policy_funcblock = {
